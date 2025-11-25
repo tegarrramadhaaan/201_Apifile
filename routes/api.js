34e8bcf7a79b1a366b03e4152d51bf79ec4c7d10
tegarrramadhaaan <anakrama2000@gmail.com>
2025-11-25 13:13:49 +0700

    Integrasi update komik dengan upload file

--- routes/api.js
+++ routes/api.js
@@ -4,3 +4,10 @@ const multer = require('multer');
 const komikController = require('../controllers/komikController');
 const upload = multer({ storage: multer.memoryStorage() });
 
+router.post('/komik', upload.single('image'), komikController.createKomik);
+router.get('/komik', komikController.getAllKomik);
+router.get('/komik/:id', komikController.getKomikById);
+router.put('/komik/:id', upload.single('image'), komikController.updateKomik);
+router.delete('/komik/:id', komikController.deleteKomik);
+
+module.exports = router;
